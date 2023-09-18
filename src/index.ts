7720bc975dbb9f452e9d27117723c0584819314d
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-09-17 20:12:11 -0400

    feat: manage VElements children

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
-import { VElement } from '../types'
+import { VElement, VNodeList } from '../types'
 
-function createVElement(tag: any, config: any): VElement {
+function createVElement(
+	tag: any,
+	config: any,
+	children: VNodeList = []
+): VElement {
 	const { className } = config
 	return {
 		tag,
+		props: {
+			children: children,
+		},
 		className: className,
 		dom: null,
 	}
 }
 
 function mountVElement(vElement: VElement, parentDOMNode: HTMLElement) {
-	const { tag, className } = vElement
+	const { tag, className, props } = vElement
 
 	const domNode = document.createElement(tag)
 	vElement.dom = domNode
+
+	if (props?.children) {
+		props.children.forEach((child) =>
+			mountVElement(child as VElement, domNode)
+		)
+	}
 
 	if (className !== undefined) {
 		domNode.className = className
@@ -21,3 +34,11 @@ function mountVElement(vElement: VElement, parentDOMNode: HTMLElement) {
 
 	parentDOMNode.appendChild(domNode)
 }
+
+const root = document.body
+const myApp = createVElement('div', { className: 'my-class' }, [
+	createVElement('h1', { className: 'my-header' }),
+	createVElement('p', { className: 'my-paragraph' }),
+])
+
+mountVElement(myApp, root)
--- types.ts
+++ types.ts
@@ -3,7 +3,11 @@ export type VNodeList = VNode[]
 
 export type VElement = {
 	tag: string
+	props?: {
+		children?: VNodeList
+	}
 	className?: string
+	style?: string
 	dom: HTMLElement | null
 }
 
